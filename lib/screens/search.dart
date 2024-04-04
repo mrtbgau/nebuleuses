54ca134d24000fbe8d6f911008838094fae9994f
mrtbgau <mrt.bgau@gmail.com>
2024-04-04 21:18:40 +0200

    Update Search screen to use Google Maps; replace hardcoded API key with build gradle variable
    * In `android/app/build.gradle`, replace manifestPlaceholders with resValue and secureProps to load GOOGLE\_MAPS\_API\_KEY from .env file
    * In `android/app/src/main/AndroidManifest.xml`, replace ${mapsApiKey} with @string/maps\_api\_key
    * In `lib/screens/search.dart`, replace BackgroundImage with GoogleMap; add _controller, _kGooglePlex, _kLake, and _goToTheLake function for GoogleMap usage; import 'package:google\_maps\_flutter/google\_maps\_flutter.dart'

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,16 +1,50 @@
-import 'package:flutter/material.dart';
-import 'package:nebuleuses/widgets/background_image.dart';
+import 'dart:async';
 
-class Search extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Search extends StatefulWidget {
   const Search({super.key});
 
   @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static const CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [BackgroundImage()],
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: const Text('To the lake!'),
+        icon: const Icon(Icons.directions_boat),
       ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
