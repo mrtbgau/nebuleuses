c954e34742dc6a9757552da971b759a43009b7a1
mrtbgau <mrt.bgau@gmail.com>
2024-06-18 21:26:34 +0200

    "Added debugShowCheckedModeBanner: false to MaterialApp in MyApp"

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: AppRouter.routes,
       home: const Splash(),
     );
